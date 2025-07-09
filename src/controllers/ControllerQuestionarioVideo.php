4e663ea1ae7f1f2902791b3fe6d585359aa236fc
GamePlayHS <54159765+GamePlayHS@users.noreply.github.com>
2025-07-08 21:30:35 -0300

    tratado questionário

--- src/controllers/ControllerQuestionarioVideo.php
+++ src/controllers/ControllerQuestionarioVideo.php
@@ -3,16 +3,41 @@
 namespace Src\Controllers;
 
 use core\Controller;
+use core\Database;
 
 class ControllerQuestionarioVideo extends Controller {
 
     public function show($args) {
+        $oConexao = Database::getInstance();
+        $codigoCurso = $args['curso'];
+        $codigoVideo = $args['video'];
+
+        // Busca o questionário do vídeo, se existir
+        $stmt = $oConexao->prepare("SELECT * FROM tbquestionario WHERE curcodigo = :curso AND vidcodigo = :video");
+        $stmt->bindValue(':curso', $codigoCurso, \PDO::PARAM_INT);
+        $stmt->bindValue(':video', $codigoVideo, \PDO::PARAM_INT);
+        $stmt->execute();
+        $questionario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if ($questionario) {
+            // Se existe, trata como alteração
+            $titulo = 'Alterar Questionário';
+            $visualizacao = false;
+            $action = '/questionario/alterar/' . $questionario['quescodigo'];
+        } else {
+            // Se não existe, trata como inclusão
+            $titulo = 'Incluir Questionário';
+            $visualizacao = false;
+            $action = '/questionario/incluir';
+        }
+
         $this->render('ViewManutencaoQuestionario', [
-            'titulo' => 'Incluir Questionário',
-            'codigoCurso' => $args['curso'],
-            'codigoVideo' => $args['video'],
-            'action' => '/questionario/incluir',
-            'visualizacao' => false,
+            'titulo' => $titulo,
+            'codigoCurso' => $codigoCurso,
+            'codigoVideo' => $codigoVideo,
+            'action' => $action,
+            'visualizacao' => $visualizacao,
+            'questionario' => $questionario
         ]);
     }
 
